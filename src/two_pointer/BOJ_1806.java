0c0663bcff8dae38cf69bf2a1107bd413f10828b
HONG0805 <112541200+HONG0805@users.noreply.github.com>
2025-09-01 17:41:17 +0900

    9/1 two_pointer

--- src/two_pointer/BOJ_1806.java
+++ src/two_pointer/BOJ_1806.java
@@ -1,5 +1,35 @@
 package two_pointer;
 
-public class BOJ_1806 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BOJ_1806 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		long S = Integer.parseInt(st.nextToken());
+
+		int[] a = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			a[i] = Integer.parseInt(st.nextToken());
+
+		int ans = Integer.MAX_VALUE;
+
+		int left = 0;
+		int sum = 0;
+		for (int right = 0; right < N; right++) {
+			sum += a[right];
+
+			while (sum >= S) {
+				ans = Math.min(ans, right - left + 1);
+				sum -= a[left++];
+			}
+		}
+		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
+	}
 }
--- src/two_pointer/BOJ_2018.java
+++ src/two_pointer/BOJ_2018.java
@@ -1,5 +1,31 @@
 package two_pointer;
 
-public class BOJ_2018 {
+import java.util.Scanner;
 
+public class BOJ_2018 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 0;
+
+		int start = 1;
+		int end = 1;
+		int sum = 1;
+
+		while (start <= N) {
+			if (sum < N) {
+				end++;
+				sum += end;
+			} else if (sum > N) {
+				sum -= start;
+				start++;
+			} else {
+				count++;
+				end++;
+				sum += end;
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
 }
