20a0b083d6cfbd7eadf217cce01c96fe247a1a83
HONG0805 <112541200+HONG0805@users.noreply.github.com>
2025-09-02 18:50:05 +0900

    9/2 two_pointer

--- src/two_pointer/BOJ_2805.java
+++ src/two_pointer/BOJ_2805.java
@@ -1,5 +1,45 @@
 package two_pointer;
 
-public class BOJ_2805 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BOJ_2805 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+
+		int[] trees = new int[N];
+		st = new StringTokenizer(br.readLine());
+		int maxHeight = 0;
+		for (int i = 0; i < N; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			maxHeight = Math.max(maxHeight, trees[i]);
+		}
+
+		int low = 0;
+		int high = maxHeight;
+		int answer = 0;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			long sum = 0;
+
+			for (int h : trees) {
+				if (h > mid) {
+					sum += h - mid;
+				}
+			}
+			if (sum >= M) {
+				answer = mid;
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		System.out.println(answer);
+	}
 }
