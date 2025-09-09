1982e5049cddcb34ea234504cdbaa7860d4dbdcd
HONG0805 <112541200+HONG0805@users.noreply.github.com>
2025-09-09 19:29:27 +0900

    9/9 greedy_1

--- src/greedy/BOJ_11399.java
+++ src/greedy/BOJ_11399.java
@@ -1,5 +1,33 @@
 package greedy;
 
-public class BOJ_11399 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class BOJ_11399 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] P = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(P);
+
+		int sum = 0;
+		int prefix = 0;
+
+		for (int i = 0; i < N; i++) {
+			prefix += P[i];
+			sum += prefix;
+		}
+
+		System.out.println(sum);
+	}
 }
--- src/greedy/BOJ_1541.java
+++ src/greedy/BOJ_1541.java
@@ -1,5 +1,33 @@
 package greedy;
 
-public class BOJ_1541 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class BOJ_1541 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String expression = br.readLine();
+
+		String[] minusSplit = expression.split("-");
+
+		int result = 0;
+
+		result += sumOfGroup(minusSplit[0]);
+
+		for (int i = 1; i < minusSplit.length; i++) {
+			result -= sumOfGroup(minusSplit[i]);
+		}
+
+		System.out.println(result);
+	}
+
+	private static int sumOfGroup(String group) {
+		String[] plusSplit = group.split("\\+");
+		int sum = 0;
+		for (String num : plusSplit) {
+			sum += Integer.parseInt(num);
+		}
+		return sum;
+	}
 }
